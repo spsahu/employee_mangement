ab4c9f33761699d8e8ca92626bd33b910566c260
spsahu <sprava198@gmail.com>
2023-09-27 21:18:56 +0530

    first commit

---
+++ src/test/java/com/employeemanagement/employeedetails/EmployeedetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeemanagement.employeedetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeedetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
